d52646e5e37e95d89e3db20079a916b90a1b09f5
Nikhil Yadav <139679118+ItsYadavNikhil@users.noreply.github.com>
2026-05-25 12:02:05 +0530

    Time: 35 ms (57.25%), Space: 14.1 MB (91%) - LeetHub

---
+++ 0923-3sum-with-multiplicity/0923-3sum-with-multiplicity.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int threeSumMulti(vector<int>& arr, int target) {
+        int n = arr.size();
+        int ans = 0;
+        int mod = 1e9 + 7;
+        sort(arr.begin(),arr.end());
+        for(int i =0;i<n;i++){
+            int left = i+1, right = n-1;
+            while(left<right){
+                int sum = arr[i]+arr[left]+arr[right];
+                if(sum == target){
+                    if(arr[left]==arr[right]){
+                        int len = right - left + 1;
+                        ans += (len*(len-1)/2);
+                        ans %= mod;
+                        break;
+                    }
+                    int freqL = 0, leftval = arr[left];
+                    int freqR = 0, rightval = arr[right];
+                    while(arr[left]==leftval){
+                        left++; freqL++;
+                    }
+                    while(arr[right]==rightval){
+                        right--; freqR++;
+                    }
+                    ans+=(freqL*freqR);
+                    ans%=mod;
+                }
+                else if (sum>target) right--;
+                else left++;
+            }
+        }
+        return ans;
+    }
+};
