bd29350b88b374b8e660f87ff6a795c26d819a17
Nikhil Yadav <139679118+ItsYadavNikhil@users.noreply.github.com>
2026-06-09 10:05:51 +0530

    Time: 1 ms (67.29%), Space: 118 MB (81.01%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,41 +1,64 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// First Approach
+
+// class Solution {
+// public:   
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* prev = nullptr;
+//         ListNode* temp = head;
+        
+//         while (temp != nullptr) {
+//             ListNode* newNode = new ListNode(temp->val);
+//             newNode->next = prev;
+//             prev = newNode;
+//             temp = temp->next;
+//         }
+        
+//         return prev;
+//     }
+    
+//     bool isPalindrome(ListNode* head) {
+//         ListNode* temp = head;
+//         ListNode* rev = reverseList(head);
+//         while(temp != nullptr){
+//             if(temp->val != rev->val) {
+//                 return false;
+//             }
+//             temp = temp->next;
+//             rev = rev->next;
+//         }
+//         return true;
+//     }
+// };
+
+//Second Approach
 class Solution {
 public:
-
-    
-    ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;
-        ListNode* temp = head;
-        
-        while (temp != nullptr) {
-            ListNode* newNode = new ListNode(temp->val);
-            newNode->next = prev;
-            prev = newNode;
-            temp = temp->next;
-        }
-        
-        return prev;
-    }
-    
     bool isPalindrome(ListNode* head) {
-        ListNode* temp = head;
-        ListNode* rev = reverseList(head);
-        while(temp != nullptr){
-            if(temp->val != rev->val) {
-                return false;
-            }
-            temp = temp->next;
-            rev = rev->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next; fast = fast->next->next;
+        }   
+        ListNode* newHead = slow;
+        ListNode* rev = reverse(newHead);
+        ListNode* left = head;
+        ListNode* right = rev;
+        while (right != nullptr) {
+            if (left->val != right->val) return false;
+            left = left->next;
+            right = right->next;
         }
         return true;
+    }
+    ListNode* reverse(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* prev = nullptr;
+        while (temp!= nullptr) {
+            ListNode* second = temp->next;
+            temp->next = prev;
+            prev  = temp;
+            temp = second;
+        }
+        return prev;
     }
 };
