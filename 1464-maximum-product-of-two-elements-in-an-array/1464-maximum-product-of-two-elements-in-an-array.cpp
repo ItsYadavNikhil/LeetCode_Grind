aa269dd27b2c141066cbea0f3cad6171dc93796a
Nikhil Yadav <139679118+ItsYadavNikhil@users.noreply.github.com>
2026-07-27 13:16:29 +0530

    Time: 0 ms (100%), Space: 13.5 MB (60.75%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size(),p,q;
         sort(nums.begin(),nums.end());
         p=(nums[n-1]-1)*(nums[n-2]-1);
-        q=(nums[0]-1)*(nums[1]-1);
+        q=(nums[0]-1)*(nums[n-1]-1);
         return max(p,q);
     }
 };
