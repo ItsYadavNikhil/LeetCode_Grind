05e9ba6c29ce96fe9117c4161c53bb1e63855435
Nikhil Yadav <139679118+ItsYadavNikhil@users.noreply.github.com>
2026-08-31 16:10:38 +0530

    Time: 0 ms (100%), Space: 8.1 MB (23.44%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,11 +1,25 @@
+// First Approach I got, not very Optimal
+
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         for (int i = 0; i < s.length(); i++) {
+//             s = s + s[0];
+//             s.erase(0,1);
+//             if(s == goal) return true;
+//         }
+//         return false;
+//     }
+// };
+
+// Optimal Approach
+
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        for (int i = 0; i < s.length(); i++) {
-            s = s + s[0];
-            s.erase(0,1);
-            if(s == goal) return true;
+        if (s.size() != goal.size()) {
+            return false;
         }
-        return false;
+        return (s + s).find(goal) != string::npos;
     }
 };
