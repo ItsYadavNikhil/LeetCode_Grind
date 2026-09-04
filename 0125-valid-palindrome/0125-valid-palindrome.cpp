255e5534a8c199cf3d80946bd7c7d3083e847885
Nikhil Yadav <139679118+ItsYadavNikhil@users.noreply.github.com>
2026-09-04 18:01:50 +0530

    Time: 4 ms (14.06%), Space: 10 MB (52.83%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
-        transform(s.begin(), s.end(), s.begin(), [](unsigned char c) {
-        return std::tolower(c);
-        });
-        for (int i = 0; i < s.size(); ) { 
-            char ch = s[i];
-            if (ch >= 'a' && ch <= 'z' || isdigit(ch)) {
-                i++; 
-            } else {
-              s.erase(i, 1); 
-            }
-        }
-        string s1 = s;
-        reverse(s.begin(),s.end());
-        cout << s;
-        cout << s1;
-        return s == s1 ? true : false;
-    }
+       int start=0;
+       int end=s.size()-1;
+       while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
+}
 };
