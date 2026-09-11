6988211452ebb2372abf871529dd6b620ba0373e
Nikhil Yadav <139679118+ItsYadavNikhil@users.noreply.github.com>
2026-09-12 01:05:16 +0530

    Time: 0 ms (100%), Space: 8.1 MB (31.88%) - LeetHub

--- 3136-valid-word/3136-valid-word.cpp
+++ 3136-valid-word/3136-valid-word.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isValid(string word) {
         if(word.size()<3) return false;
-        string vowel = "aeiouAEIOU";
+        static constexpr string_view vowel = "aeiouAEIOU";
         bool vflag = false, cflag = false;
         for (unsigned char c : word) {
             if (!isalnum(c)) {
