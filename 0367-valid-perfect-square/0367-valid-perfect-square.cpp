dd2830ad0ed8f2fb6f8daf90a5e6fcd22c6644ba
Nikhil Yadav <139679118+ItsYadavNikhil@users.noreply.github.com>
2026-09-12 01:37:21 +0530

    Time: 0 ms (100%), Space: 7.8 MB (17.76%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPerfectSquare(int num) {
         if (num < 2) return true;
-        int sqrt = 0, left = 0, right = num/2;
+        int sqrt = 0, left = 1, right = num/2;
         while(left<=right) {
             int mid = left + (right - left)/2;
             if(mid <= num/mid) {
